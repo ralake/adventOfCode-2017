7dd0d8f5c38c10a53e25982815cb4584af93c4aa
Richard Lake <ralake1985@gmail.com>
2017-12-10 21:20:56 +0000

    Day six part two

--- __tests__/daySix.test.js
+++ __tests__/daySix.test.js
@@ -3,7 +3,10 @@ const memoryReallocation = require('../lib/daySix')
 describe('memoryReallocation', () => {
   describe('detectInfiniteLoop', () => {
     it('returns the amount of cycles needed before an infinite loop is detected', () => {
-      expect(memoryReallocation.detectInfiniteLoop('0 2 7 0').cycles).toEqual(5)
+      expect(memoryReallocation.detectInfiniteLoop('5 1 10 0 1 7 13 14 3 12 8 10 7 12 0 6').cycles).toEqual(5042)
+    })
+
+    it('returns the amount of cycles between the first and second occurance of a repetition', () => {
       expect(memoryReallocation.detectInfiniteLoop('5 1 10 0 1 7 13 14 3 12 8 10 7 12 0 6').cycles).toEqual(5042)
     })
   })
